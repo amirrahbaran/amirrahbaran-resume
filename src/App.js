7ed9eff5665740f8605976e74713b8ea856e63da
AmirReza Rahbaran <amirrezarahbaran@gmail.com>
2019-01-21 17:03:34 +0330

    contents populated and the page designed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import data from "./data.json";
+import "./App.css";
+import { SocialIcon } from "react-social-icons";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="navigation" />
+        <div className="fullpage first">
+          <h1 className="title">{data.title}</h1>
+          <div>
+            <h2>{data.subtitle}</h2>
+          </div>
+          <div className="icons-wrapper">
+            {Object.keys(data.links).map(key => {
+              return (
+                <div className="icon">
+                  <SocialIcon url={data.links[key]} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="fullpage second">
+          <h3>{data.sections[0].title}</h3>
+          <div className="paragraphs">
+            {data.sections[0].items.map(p => {
+              return <p>{p.content}</p>;
+            })}
+          </div>
+        </div>
+        <div className="fullpage third">
+          <h3>{data.sections[1].title}</h3>
+          <div className="cards-wrapper">
+            {data.sections[1].items.map(skill => {
+              return (
+                <div className="card">
+                  <div className="image-wrapper">
+                    <img src={skill.content.image} alt={skill.content.title} />
+                  </div>
+                  <div className="skill-title-wrapper">
+                    <h4>{skill.content.title}</h4>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     );
   }
