df50c188761053f8708a6a9141970416c76ffecd
AmirReza Rahbaran <amirrezarahbaran@gmail.com>
2019-01-22 14:52:16 +0330

    SkillCard component added

--- src/sections/SkillSection.js
+++ src/sections/SkillSection.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Fullpage from "../components/Fullpage";
 import data from "../data.json";
+import SkillCard from "../components/SkillCard";
 
 class SkillSection extends Component {
   render() {
@@ -8,18 +9,9 @@ class SkillSection extends Component {
       <Fullpage className="third">
         <h3>{data.sections[1].title}</h3>
         <div className="cards-wrapper">
-          {data.sections[1].items.map(skill => {
-            return (
-              <div className="card">
-                <div className="image-wrapper">
-                  <img src={skill.content.image} alt={skill.content.title} />
-                </div>
-                <div className="skill-title-wrapper">
-                  <h4>{skill.content.title}</h4>
-                </div>
-              </div>
-            );
-          })}
+          {data.sections[1].items.map(eachSkill => 
+            <SkillCard skill={eachSkill} />
+          )}
         </div>
       </Fullpage>
     );
