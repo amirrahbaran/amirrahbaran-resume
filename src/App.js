a9c9874a9d19f130c7776f8e895c1adbc7d9bc3e
AmirReza Rahbaran <amirrezarahbaran@gmail.com>
2019-01-22 14:24:22 +0330

    About and Skill sections component added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
 import React, { Component } from "react";
-import data from "./data.json";
-import "./App.css";
+
 import IntroductionSection from "./sections/IntroductionSection";
+import AboutSection from "./sections/AboutSection";
+import SkillSection from "./sections/SkillSection.js";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="navigation" />
-
         <IntroductionSection />
-
-        <div className="fullpage second">
-          <h3>{data.sections[0].title}</h3>
-          <div className="paragraphs">
-            {data.sections[0].items.map(p => {
-              return <p>{p.content}</p>;
-            })}
-          </div>
-        </div>
-        <div className="fullpage third">
-          <h3>{data.sections[1].title}</h3>
-          <div className="cards-wrapper">
-            {data.sections[1].items.map(skill => {
-              return (
-                <div className="card">
-                  <div className="image-wrapper">
-                    <img src={skill.content.image} alt={skill.content.title} />
-                  </div>
-                  <div className="skill-title-wrapper">
-                    <h4>{skill.content.title}</h4>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+        <AboutSection />
+        <SkillSection />
       </div>
     );
   }
