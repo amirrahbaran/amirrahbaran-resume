221c242ae876c60841484000d4802e30cdd44221
rahbaran <amirrezarahbaran@gmail.com>
2019-01-27 18:15:29 +0330

    Snow added and section colors changed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigator from "./components/Navigator";
 import IntroductionSection from "./sections/IntroductionSection";
 import AboutSection from "./sections/AboutSection";
 import SkillSection from "./sections/SkillSection";
+import SnowStorm from "react-snowstorm";
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <SnowStorm />
         <Navigator />
         <IntroductionSection />
         <AboutSection />
